713f3d4cbc9735250a8512c6c29e1b3b385908d1
patrickzw7 <patrickzwchen@gmail.com>
2024-03-08 03:35:32 -0500

    change of SQL solution

--- Database/1308-Running Total for Different Genders.sql
+++ Database/1308-Running Total for Different Genders.sql
@@ -1,7 +1,11 @@
-SELECT s2.gender, s2.day,
-       SUM(s1.score_points) AS total
-FROM Scores s1 
-JOIN Scores s2
-ON s1.gender = s2.gender AND s2.day >= s1.day
-GROUP BY s2.gender, s2.day
-ORDER BY s2.gender, s2.day;
+# Write your MySQL query statement below
+
+SELECT s.gender,
+       s.day,
+       SUM(s.score_points) OVER (
+        PARTITION BY s.gender
+        ORDER BY s.day ASC
+    ) AS total
+FROM Scores s
+GROUP BY s.gender, s.day
+ORDER BY gender ASC, day ASC;
